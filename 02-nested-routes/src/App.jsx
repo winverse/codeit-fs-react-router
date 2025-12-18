d9af490f5b234cc2fadc7d2abd417ab2e9ce6e43
winverse <public.winverse@gmail.com>
2025-12-19 05:58:09 +0900

    feat: 02-nested-routes

--- 02-nested-routes/src/App.jsx
+++ 02-nested-routes/src/App.jsx
@@ -2,15 +2,13 @@ import { Routes, Route } from 'react-router';
 import { HomePage } from './features/home/HomePage';
 import { HomeLayout } from './layouts/HomeLayout/HomeLayout';
 import { CommunityNavbar } from './features/community/CommunityNavbar';
-import { CommunityList } from './features/community/CommunityList';
-import { NewPostPage } from './features/post/NewPostPage';
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<HomeLayout />}>
         <Route index={true} element={<HomePage />} />
-        <Route path="community" element={<CommunityNavbar />} /> 
+        <Route path="community" element={<CommunityNavbar />} />
       </Route>
     </Routes>
   );
