fff7d721a5771359c5b1adf076e25072345243ee
winverse <public.winverse@gmail.com>
2025-12-19 14:20:08 +0900

    feat: ./06.programmatic-navigation

--- 06.programmatic-navigation/src/App.jsx
+++ 06.programmatic-navigation/src/App.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from 'react-router';
-import { HomePage } from './pages/Home';
-import { LoginPage } from './pages/Login';
-import { DashboardPage } from './pages/Dashboard';
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Dashboard } from './pages/Dashboard';
 
 export function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   );
 }
